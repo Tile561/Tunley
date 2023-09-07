52d0e6720795db02e195050b829003d03bcb8ddc
Tile561 <james.q.merilien@gmail.com>
2023-09-07 16:33:53 -0400

    Setup the table view

--- lab-tunley/TracksViewController.swift
+++ lab-tunley/TracksViewController.swift
@@ -7,18 +7,42 @@
 
 import UIKit
 
-class TracksViewController: UIViewController {
+class TracksViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell with identifier, "TrackCell"
+        // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
+
+        // Get the track that corresponds to the table view row
+        let track = tracks[indexPath.row]
+
+        // Configure the cell with it's associated track
+        cell.configure(with: track)
+
+        // return the cell for display in the table view
+        return cell
+        
+    }
+    
     
     var tracks: [Track] = []
     // TODO: Pt 1 - Add a tracks property
 
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     // TODO: Pt 1 - Add table view outlet
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tracks = Track.mockTracks
         print(tracks)
+        
+        tableView.dataSource = self
         // TODO: Pt 1 - Set tracks property with mock tracks array
 
         
